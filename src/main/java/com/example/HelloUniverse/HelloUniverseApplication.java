c164991bd750f5a837133d92164455c2d359b69d
pradip <pradipvdeore@gmail.com>
2021-05-20 15:29:08 +0530

    Class name issue fixed

--- src/main/java/com/example/HelloUniverse/HelloUniverseApplication.java
+++ src/main/java/com/example/HelloUniverse/HelloUniverseApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class wqHelloUniverseApplication {
+public class HelloUniverseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloUniverseApplication.class, args);
