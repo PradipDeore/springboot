631c4e7cf17cf8ec18222bafb60d11aeb942cd83
pradip <pradipvdeore@gmail.com>
2021-05-20 15:22:10 +0530

    push code

---
+++ src/main/java/com/example/HelloUniverse/HelloUniverseApplication.java
@@ -0,0 +1,13 @@
+package com.example.HelloUniverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class wqHelloUniverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloUniverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HelloUniverse/HelloUniverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelloUniverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloUniverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
